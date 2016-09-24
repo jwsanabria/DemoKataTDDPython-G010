b95b0b3f46ff2ffb105e2673c34f4194f3c6c440
jwsanabria <jwsanabria@gmail.com>
2016-09-24 12:13:06 -0500

    Iteracion 1 - Ciclo TDD para que funcione con un string de N números (rojo)

--- SecuenciaTest.py
+++ SecuenciaTest.py
@@ -11,3 +11,6 @@ class SecuenciaTest(TestCase):
 
     def test_estadisticas_cadenaConDosNumeros(self):
         self.assertEqual(Secuencia().estadisticas("1,3"), [2, ], "Cadena con dos numeros")
+
+    def test_estadisticas_cadenaConVariosNumeros(self):
+        self.assertEqual(Secuencia().estadisticas("1,3,5,6,7"), [5, ], "Cadena con dos numeros")
