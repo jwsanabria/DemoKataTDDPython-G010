9f95fd07a023bf57f1d438e9a1c06a9f46ff2c8f
jwsanabria <jwsanabria@gmail.com>
2016-09-24 12:06:25 -0500

    Iteracion 1 - Ciclo TDD para que funcione con un string de 1 número (rojo)

--- SecuenciaTest.py
+++ SecuenciaTest.py
@@ -5,3 +5,6 @@ from Secuencia import Secuencia
 class SecuenciaTest(TestCase):
     def test_estadisticas(self):
         self.assertEqual(Secuencia().estadisticas(""), [0,], "Cadena vacia")
+
+    def test_estadisticas_cadenaConUnNumero(self):
+        self.assertEqual(Secuencia().estadisticas("1"), [1, ], "Cadena con un numero")
