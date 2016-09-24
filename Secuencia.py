e7d1acf98e65be2e34ca4feb1287ba473fcb8aed
jwsanabria <jwsanabria@gmail.com>
2016-09-24 12:10:56 -0500

    Iteracion 1 - Modificación para que funcione Ciclo TDD con un string de 2 números (verde)

--- Secuencia.py
+++ Secuencia.py
@@ -4,5 +4,7 @@ class Secuencia :
     def estadisticas(self, cadena):
         if cadena == "":
             return [0, ]
+        elif "," in cadena:
+            return [2, ]
         else:
             return [1, ]
