6974396380008ae5ef3e08c1df5bb82423ae495a
dianaespc <dpaolac@gmail.com>
2016-09-24 15:24:22 -0500

    Iteracion 2 - Ajuste para que funcione Ciclo TDD con el string vacio (verde)

--- Secuencia.py
+++ Secuencia.py
@@ -3,7 +3,7 @@ __author__ = 'Grupo10'
 class Secuencia :
     def estadisticas(self, cadena):
         if cadena == "":
-            return [0, ]
+            return [0, 0]
         elif "," in cadena:
             numeros = cadena.split(",")
             return [len(numeros), ]
