bd75d9b1fd6ad9c1d9a7f542466491be41079158
dianaespc <dpaolac@gmail.com>
2016-09-24 22:28:48 -0500

    Iteracion 4 - Ajustes para que funcione el Ciclo TDD con un string de N números (verde)

--- Secuencia.py
+++ Secuencia.py
@@ -9,6 +9,9 @@ class Secuencia :
             if len(numeros) == 2:
                 return [len(numeros), int(min(numeros)), int(max(numeros)), (int(numeros[0])+int(numeros[1]))/2]
             else:
-                return [len(numeros), int(min(numeros)), int(max(numeros))]
+                suma = 0.0
+                for i in range (0,len(numeros)):
+                    suma = suma + int(numeros[i])
+                return [len(numeros), int(min(numeros)), int(max(numeros)), suma/len(numeros)]
         else:
             return [1, int(cadena), int(cadena), int(cadena)]
